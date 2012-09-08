5fa4e3adf286d0cb73afd362a47f9ddb97c0c140
Kovak <kovac1066@gmail.com>
2012-09-07 20:15:20 -0600

    New Art assets

--- forandroid/main.py
+++ forandroid/main.py
@@ -10,8 +10,223 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.factory import Factory
+import time
+
+class Subtile():
+    def __init__(self,atlas,material_name,position,frames):
+        self.atlas = atlas
+        self.material_name = material_name
+        self.position = [[True,True],[True,True]] if position is None else position
+        self.frames = frames
+
+    def __str__(self):
+        return self.material_name + ': ' + str(self.position)
+
 
 class Tile(Widget):
+    layers = NumericProperty(0)
+    update_now = BooleanProperty(False)
+
+    def __init__(self,typelist,gridpos=None,**kwargs):
+        self.typelist = typelist
+        self.gridpos = gridpos
+        self.layers = len(typelist)
+        self.source_images = []
+        self.source_frames = []
+        self.update_source_images()
+
+        super(Tile,self).__init__(**kwargs)
+
+    def on_touch_down(self,touch):
+        if not self.collide_point(touch.x, touch.y):
+            return False
+
+        # get new tile characteristics from parent class, which is aware of surrounding tiles.
+        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.tile_touch(self)
+        self.parent.tile_touch(self)
+        return True
+
+    def on_touch_move(self,touch):
+        if not self.collide_point(touch.x, touch.y):
+            return False
+
+        # get new tile characteristics from parent class, which is aware of surrounding tiles.
+        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.tile_touch(self)
+        self.parent.tile_touch(self)
+        return True
+
+
+    def update_source_images(self):
+         # sorry this is ugly guys, but it works, it's fast, and it's pretty clear what's going on.
+        for idx,t in enumerate(self.typelist):
+            if t.position == [[True,True],[True,True]]:
+                position = None
+                orientation = None
+            elif t.position == [[True,False],[False,False]]:
+                position = "3"
+                orientation = 'Interior'
+            elif t.position == [[False,True],[False,False]]:
+                position = "1"
+                orientation = 'Interior'
+            elif t.position == [[False,False],[True,False]]:
+                position = "9"
+                orientation = 'Interior'
+            elif t.position == [[False,False],[False,True]]:
+                position = "7"
+                orientation = 'Interior'
+            elif t.position == [[True,True],[False,False]]:
+                position = "2"
+                orientation = 'Interior'
+            elif t.position == [[False,False],[True,True]]:
+                position = "8"
+                orientation = 'Interior'
+            elif t.position == [[True,False],[True,False]]:
+                position = "6"
+                orientation = 'Interior'
+            elif t.position == [[False,True],[False,True]]:
+                position = "4"
+                orientation = 'Interior'
+            elif t.position == [[False,True],[True,True]]:
+                position = "3"
+                orientation = 'Exterior'
+            elif t.position == [[True,False],[True,True]]:
+                position = "1"
+                orientation = 'Exterior'
+            elif t.position == [[True,True],[False,True]]:
+                position = "9"
+                orientation = 'Exterior'
+            elif t.position == [[True,True],[True,False]]:
+                position = "7"
+                orientation = 'Exterior'
+            
+            if position is None:
+                new_source_image = t.atlas + 'Tile-' + t.material_name
+            else:
+                new_source_image = t.atlas + 'Tile-' + t.material_name + '-Position' + position + '-' + orientation
+
+            try:
+                self.source_images[idx] = new_source_image
+                self.source_frames[idx] = t.frames
+            except IndexError:
+                self.source_images.append(new_source_image)
+                self.source_frames.append(t.frames)
+
+            self.update_now = not self.update_now
+            print "source images updated to", self.source_images
+
+        self.layers = len(self.typelist)
+
+    def get_current_frame(self,layer,framecounter):
+        try:
+            return self.source_images[layer] + '-' + str(framecounter % self.source_frames[layer] + 1) if self.source_frames[layer] > 1 else self.source_images[layer]
+        except:
+            return ""
+
+    def add_segment(self,tile_type):
+        try:
+            existing_subtile_idx = [x.material_name for x in self.typelist].index(tile_type.material_name)
+        except ValueError:
+            existing_subtile_idx = None
+
+        if existing_subtile_idx is not None:
+            o = self.typelist[existing_subtile_idx].position
+            n = tile_type.position
+
+            print "This is tile",self.gridpos
+            print "Adding subtile", tile_type.material_name,", old position was ",o,"new position is",n
+
+            new_position = [[o[0][0] or n[0][0], o[0][1] or n[0][1]],
+                [o[1][0] or n[1][0], o[1][1] or n[1][1]]]
+
+            self.typelist[existing_subtile_idx].position = new_position
+
+            # now send to top
+            self.typelist[existing_subtile_idx], self.typelist[-1] = self.typelist[-1], self.typelist[existing_subtile_idx]
+        else:
+            print "This is tile",self.gridpos
+            print "Adding subtile", tile_type.material_name, "at position",tile_type.position
+            self.typelist.append(tile_type)
+
+        # if the top of the typelist is a full tile, remove the stuff under it
+        if self.typelist[-1].position == [[True, True],[True,True]]:
+            print "adjusting typelist: old is",self.typelist
+            self.typelist = [self.typelist[-1]]
+
+        self.update_source_images()
+        print "layers:",self.layers
+
+
+
+
+class AnotherOldTile(Widget):
+    parent = ObjectProperty(None)
+
+    def __init__(self,basetype,posarray=None,**kwargs):
+        if posarray is None:
+            self.posarray = [[True,True],[True,True]]
+        else:
+            self.posarray = posarray
+
+        self.basetype = basetype
+        self.update_source_image()
+
+        super(Tile,self).__init__(**kwargs)
+
+    def get_current_frame(self,framecounter):
+        return self.source_image + '-' + str(framecounter % self.basetype.frames + 1) if self.basetype.frames > 1 else self.source_image
+
+    def update_source_image(self):
+        # sorry this is ugly guys, but it works, it's fast, and it's pretty clear what's going on.
+        if self.posarray == [[True,True],[True,True]]:
+            position = None
+            orientation = None
+        elif self.posarray == [[True,False],[False,False]]:
+            position = "3"
+            orientation = 'Interior'
+        elif self.posarray == [[False,True],[False,False]]:
+            position = "1"
+            orientation = 'Interior'
+        elif self.posarray == [[False,False],[True,False]]:
+            position = "9"
+            orientation = 'Interior'
+        elif self.posarray == [[False,False],[False,True]]:
+            position = "7"
+            orientation = 'Interior'
+        elif self.posarray == [[True,True],[False,False]]:
+            position = "2"
+            orientation = 'Interior'
+        elif self.posarray == [[False,False],[True,True]]:
+            position = "8"
+            orientation = 'Interior'
+        elif self.posarray == [[True,False],[True,False]]:
+            position = "6"
+            orientation = 'Interior'
+        elif self.posarray == [[False,True],[False,True]]:
+            position = "4"
+            orientation = 'Interior'
+        elif self.posarray == [[False,True],[True,True]]:
+            position = "3"
+            orientation = 'Exterior'
+        elif self.posarray == [[True,False],[True,True]]:
+            position = "1"
+            orientation = 'Exterior'
+        elif self.posarray == [[True,True],[False,True]]:
+            position = "9"
+            orientation = 'Exterior'
+        elif self.posarray == [[True,True],[True,False]]:
+            position = "7"
+            orientation = 'Exterior'
+        
+        if position is None:
+            self.source_image = self.basetype.atlas + 'Tile-' + self.basetype.material_name
+        else:
+            self.source_image = self.basetype.atlas + 'Tile-' + self.basetype.material_name + '-Position' + position + '-' + orientation
+
+
+
+
+
+class OldTile(Widget):
     source_image = StringProperty(None)
     atlas = StringProperty(None)
     interior = StringProperty(None,allownone = True)
@@ -41,7 +256,7 @@ class Tile(Widget):
 
         self.update_source_text()
 
-        super(Tile,self).__init__(**kwargs)
+        super(OldTile,self).__init__(**kwargs)
 
     def test(self,active_frame):
         return self.source_image + '-' + str(active_frame % self.frames + 1) if self.is_animated else self.source_image
@@ -57,8 +272,8 @@ class Tile(Widget):
             return False
         
         # get new tile characteristics from parent class, which is aware of surrounding tiles.
-        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.get_new_tiles(self)
-        self.parent.get_new_tiles(self)
+        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.tile_touch(self)
+        self.parent.tile_touch(self)
         return True
 
     def on_touch_move(self,touch):
@@ -66,24 +281,36 @@ class Tile(Widget):
             return False
         
         # get new tile characteristics from parent class, which is aware of surrounding tiles.
-        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.get_new_tiles(self)
-        self.parent.get_new_tiles(self)
+        # self.atlas, self.interior, self.position_code, self.exterior, self.frames = self.parent.tile_touch(self)
+        self.parent.tile_touch(self)
         return True
 
-class Screen(GridLayout):
+class Screen(FloatLayout):
     rows = NumericProperty(6)
     cols = NumericProperty(9)
     tiles = ObjectProperty(None)
     active_frame = NumericProperty(0)
+    parent = ObjectProperty(None)
 
-    def __init__(self,parent,fill=None,**kwargs):
+    def __init__(self,tileset,fill=None,**kwargs):
         # get reference to parent (ScreenEditor) and grab atlas/tilenames
-        self.parent = parent
-        self.atlas = parent.atlas
-        self.tile_names = parent.tile_names
-        self.tile_frame_dict = parent.tile_frame_dict
+        # self.parent = parent
+        # self.atlas = parent.atlas
+        # self.tile_names = parent.tile_names
+        # self.tile_frame_dict = parent.tile_frame_dict
+
+        self.tileset = tileset
+
+        
+
 
         super(Screen,self).__init__(**kwargs)
+
+        ###This is super fucked up because width appears to be 1, even though it explicitly gets set to the right value. Looks like we need schedule_once to fix this; but I don't know why we didn't need it when using a GridLayout
+        self.tile_width = self.width/self.cols
+
+        ### it works fine if you set it to a constant afterward.
+        self.tile_width = 128
 
         #start the animation counter
         Clock.schedule_interval(self.increment_active_frame, 1.0)
@@ -92,24 +319,43 @@ class Screen(GridLayout):
         assert fill is not None
 
         if fill is not None:
-            self.tiles = [Tile(self.atlas,
-                fill,
-                fill,
-                5,
-                self.tile_frame_dict[fill],
-                self,
-                (i,j),
-                size=(self.width/self.cols,self.width/self.cols),
-                size_hint=(None,None),
-                ) for i in xrange(int(self.rows)) for j in xrange(int(self.cols))]
-            for tile in self.tiles:
-                self.add_widget(tile)
+            # self.tiles = [Tile(self.atlas,
+            #     fill,
+            #     fill,
+            #     5,
+            #     self.tile_frame_dict[fill],
+            #     self,
+            #     (i,j),
+            #     size=(self.width/self.cols,self.width/self.cols),
+            #     size_hint=(None,None),
+            #     ) for i in xrange(int(self.rows)) for j in xrange(int(self.cols))]
+            self.tiles = [Tile([self.tileset[0]],parent=self,size=(self.tile_width,self.tile_width), x = self.x + self.tile_width*j,y=self.y+self.tile_width*i,size_hint = (None,None),gridpos = (i,j)) for i in xrange(int(self.rows)) for j in xrange(int(self.cols))]
+            for ctile in self.tiles:
+                self.add_widget(ctile)
 
 
     def increment_active_frame(self, dt):
         self.active_frame += 1
 
-    def get_new_tiles(self,tile):
+    def tile_touch(self,tile):
+        new_base_type = self.parent.current_tile_type
+        all_surrounding_tiles = self.get_surrounding_tiles(*tile.gridpos)
+
+        position_matrix = [
+            [[[False,True],[False,False]],[[True,True],[False,False]],[[True,False],[False,False]]],
+            [[[False,True],[False,True]],[[True,True],[True,True]],[[True,False],[True,False]]],
+            [[[False,False],[False,True]],[[False,False],[True,True]],[[False,False],[True,False]]]]
+
+        for r,row in enumerate(position_matrix):
+            for c,position in enumerate(row):
+                active_tile = all_surrounding_tiles[r][c]
+
+                if active_tile is None:
+                    continue
+
+                active_tile.add_segment(Subtile(new_base_type.atlas,new_base_type.material_name,position,new_base_type.frames))
+
+    def tile_touch_OBSELETE(self,tile):
         new_base_type = self.parent.current_tile_type
 
         all_surrounding_tiles = self.get_surrounding_tiles(*tile.gridpos)
@@ -200,21 +446,18 @@ class ScreenEditor(FloatLayout):
     spacing = NumericProperty(10)
     level = ObjectProperty(None)
 
-    current_tile_type = StringProperty(None)
-    current_tile_frames = NumericProperty(1)
-
-    def __init__(self, atlas, tile_names, tile_frame_dict, **kwargs):
-        self.atlas = atlas
-        self.tile_names = tile_names
-        self.tile_frame_dict = tile_frame_dict
-        self.current_tile_type = tile_names[0]
+    def __init__(self, tileset, **kwargs):
+        self.tileset = tileset
+        self.current_tile_type = self.tileset[0]
 
         super(ScreenEditor,self).__init__(**kwargs)
         
-        self.draw_buttons(tile_names)
+        self.draw_buttons([x.material_name for x in self.tileset])
 
-        self.screen = Screen(self,
-            fill=tile_names[0],
+        self.screen = Screen(
+            self.tileset,
+            parent = self,
+            fill = self.current_tile_type,
             x = self.x + self.button_width + 2*self.spacing,
             y = self.y + self.spacing,
             width = self.width - self.button_width - 3*self.spacing,
@@ -222,6 +465,7 @@ class ScreenEditor(FloatLayout):
             spacing = 0,
             padding = 0,
             )
+
 
         self.add_widget(self.screen)
 
@@ -248,18 +492,23 @@ class ScreenEditor(FloatLayout):
 
     def button_callback(self, instance):
 
-        if instance.text in self.tile_names:
-            self.current_tile_type = instance.text
+        if instance.text in [x.material_name for x in self.tileset]:
+            self.current_tile_type = self.tileset[[x.material_name for x in self.tileset].index(instance.text)]
 
 class ScreenEditorWidget(Widget):
     def __init__(self):
         super(ScreenEditorWidget,self).__init__()
+        
+        tileset = [Subtile('atlas://ArtAssets/TileSets/Tileset-SeaGrass_Forest_128/','Grass',None,1),
+            Subtile('atlas://ArtAssets/TileSets/Tileset-SeaGrass_Forest_128/','Sand',None,1),
+            Subtile('atlas://ArtAssets/TileSets/Tileset-SeaGrass_Forest_128/','Water',None,4),
+            Subtile('atlas://ArtAssets/TileSets/Tileset-SeaGrass_Forest_128/','Dirt',None,1)]
+
         mainwidget = ScreenEditor(
-            'atlas://ArtAssets/TileSets/SeaGrass_Forest_Tileset_128/',
-            ['Grass','Sand','Water','Dirt'], 
-            {'Grass': 1,'Sand': 1,'Water': 4, 'Dirt': 1},
+            tileset,
             pos = (0, 0), 
             size = (Window.width, Window.height))
+
         self.add_widget(mainwidget)
 
 
