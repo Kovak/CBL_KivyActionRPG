89d0077c526532ac973f93f2424854329888ba26
Kerby Smith <kerby@Romulus.(none)>
2012-09-20 14:48:33 -0600

    updated

--- ParticleEngine/main.py
+++ ParticleEngine/main.py
@@ -14,9 +14,13 @@ import re
 
 class Particle(Widget):
     
-    color = ListProperty([0,.8,.7,1.])
+    color = ListProperty([1.,1.,1.,1.])
     textures = ObjectProperty(None)
     active_frame = NumericProperty(0)
+    duration = NumericProperty(0)
+    dir_vector = ListProperty([])
+    frame_delay = NumericProperty(0)
+    warehouse = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         # remove when there are more ways of calling Particle
@@ -24,23 +28,47 @@ class Particle(Widget):
         super(Particle,self).__init__(**kwargs)
         self.frames = len(self.textures) if self.textures is not None else 1
 
-        # if self.frames > 1:
-        #     Clock.schedule_interval(self.increment_frame,frame_delay)
-        # if duration > 0:
-        #     anim = Animation(pos = (self.x + dir_vector[0], self.y + dir_vector[1]), duration=duration)
-        #     anim.bind(on_complete = self.delete)
-        #     anim.start(self)
+    def animate_particle(self):
 
-    def delete(self, instance, value):
-        self.parent.remove_widget(self)
+        self.frames = len(self.textures) if self.textures is not None else 1
+        if self.frames > 1:
+            # Clock.schedule_interval(self.increment_frame,self.frame_delay)
+            Clock.schedule_once(self.increment_frame,self.frame_delay)
+        if self.duration > 0:
+            anim = Animation(pos = (self.x + self.dir_vector[0], self.y + self.dir_vector[1]), duration=self.duration)
+            anim.bind(on_complete = self.remove)
+            anim.start(self)
+        
+
+    def remove(self, instance, value):
+        self.reset()
+        self.warehouse.get_particle(self)
 
     def increment_frame(self, dt):
-        if random() < .4:
-            return
+        # if self.frame_delay == 0:
+        #     print self.active_frame
+        #     print "stopping"
+        #     return False
+        
+        
         if self.active_frame < self.frames - 1: 
             # print self.active_frame
             self.active_frame += 1
             self.color[3] *= 0.8
+            Clock.schedule_once(self.increment_frame,self.frame_delay)
+
+        # self.frame_delay *= (0.01*randint(75,125))
+        print self.active_frame
+        
+
+    def reset(self):
+        self.dir_vector = Vector(0,0)
+        self.duration = 0 
+        self.size = (0,0)
+        # self.frame_delay = 0
+        self.pos = (0,0)
+        self.active_frame = 0
+        self.color = [1.,1.,1.,1.]
 
     def on_touch_down(self,touch):
         return False
@@ -65,8 +93,11 @@ class ParticleEmitter(Widget):
     scatter_init = NumericProperty(10)
     scatter_end = NumericProperty(30)
 
-    def __init__(self,**kwargs):
+    def __init__(self,warehouse=None,**kwargs):
         super(ParticleEmitter,self).__init__(**kwargs)
+        self.warehouse = warehouse
+        self.warehouse.register_emitter(self)
+
 
         # create list of textures
         basename = 'VFX_SmokeParticle'
@@ -94,18 +125,28 @@ class ParticleEmitter(Widget):
     def emit_particle(self,dt):
         particle_width = 64
         duration = 3.
-        frame_delay = 0.1
+        frame_delay = duration/len(self.texture_list)*.9
 
-        p = Particle(textures = self.texture_list,
-            frame_delay = frame_delay,
-            x = self.x + (self.width - particle_width)/2. + randint(-self.scatter_init, self.scatter_init), 
-            y = self.y + (self.height - particle_width)/2. + randint(-self.scatter_init, self.scatter_init), 
-            dir_vector = Vector(0 + randint(-self.scatter_end, self.scatter_end),300 + randint(-self.scatter_end, self.scatter_end)),
-            duration = duration,
-            parent=self, 
-            size = (particle_width,particle_width), 
-            size_hint = (None, None))
-        self.add_widget(p)
+        particle = self.request_particle_from_warehouse()
+        if particle is not None:
+            x = self.x + (self.width - particle_width)/2. + randint(-self.scatter_init, self.scatter_init) 
+            y = self.y + (self.height - particle_width)/2. + randint(-self.scatter_init, self.scatter_init)
+            particle.warehouse = self.warehouse 
+            particle.pos = self.pos
+            particle.size = (particle_width, particle_width)
+            particle.textures = self.texture_list
+            particle.frame_delay = frame_delay
+            particle.duration = duration
+            particle.dir_vector = Vector(0 + randint(-self.scatter_end, self.scatter_end),300 + randint(-self.scatter_end, self.scatter_end))
+            particle.animate_particle()
+
+
+
+    def request_particle_from_warehouse(self):
+        return self.warehouse.send_particle_to_emitter()
+
+    def direct_particle(self):
+        pass
 
 class Particle_Warehouse():
 
@@ -116,13 +157,37 @@ class Particle_Warehouse():
             self.particles.append(Particle(dir_vector = Vector(0,0), duration = 0, size = (0,0), size_hint = (None,None), pos = (0,0)))
             self.parent.add_widget(self.particles[-1])
 
+        self.emitters =[]
+
+
+    def register_emitter(self,emitter):
+        self.emitters.append(emitter)
+        self.balance_load()
+
+    def send_particle_to_emitter(self):
+        try:
+            return self.particles.pop()
+        except IndexError:
+            return None
+        
+
+    def get_particle(self, particle):
+        self.particles.append(particle)
+
+    def balance_load(self):
+        number_of_emitters = len(self.emitters)
+        particles_for_each_emitter = len(self.particles)/number_of_emitters
+        # send_particle_to_emitter(particles)
+        pass
+
 
 class ParticleEngineApp(App):
     def build(self):
         # 'atlas://VFX/smoke_particles/VFX_SmokeParticle'
         fl = FloatLayout(pos=(0,0),size=Window.size)
         fl.add_widget(CBL_DebugPanel(pos=(0,0),size=(100,50)))
-        pw = Particle_Warehouse(5000, parent = fl)
+        pw = Particle_Warehouse(1000, parent = fl)
+        e = ParticleEmitter(warehouse=pw)
         return fl
 
 
