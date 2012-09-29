6e38e9dabf59661f72ac6edf3462de3657b229cf
Kerby Smith <kerby@Romulus.(none)>
2012-09-29 14:16:25 -0600

    added x slider, y slider and button to set params for x and y velocity before initiating the emitter. the top slider sets x and bottom sets y

--- ParticleEngine/main.py
+++ ParticleEngine/main.py
@@ -16,20 +16,18 @@ from collections import deque
 from functools import partial
 import re
 
-class Particle(InstructionGroup):
-    refresh_time = .08
+from kivy.uix.button import Button
+from kivy.uix.slider import Slider
+from kivy.uix.label import Label
+
+
+class Particle(Rectangle):
+    refresh_time = .05
     frame = 0
 
     def __init__(self, initial_params, param_changes, parent = None, dormant = False, **kwargs):
         # these parameters are necessary for Rectangle
-        super(Particle,self).__init__()
-        self.rect = Rectangle(pos=(initial_params['x'],initial_params['y']),size=(initial_params['width'],initial_params['height']),texture=initial_params['texture'])
-        self.add(self.rect)
-
-        self.color_code = (1., 1., 1., 1.)
-        self.color = Color(*self.color_code)
-        self.insert(0, self.color)
-
+        super(Particle,self).__init__(pos=(initial_params['x'],initial_params['y']),size=(initial_params['width'],initial_params['height']),texture=initial_params['texture'])
         self.initial_params = initial_params
         self.param_changes = param_changes
         self.parent = parent
@@ -40,15 +38,13 @@ class Particle(InstructionGroup):
 
 
     def activate(self):
-        self.rect.pos = (self.initial_params['x'],self.initial_params['y'])
-        self.rect.size  = (self.initial_params['width'],self.initial_params['height'])
-        self.rect.texture = self.initial_params['texture']
-
-        self.change_color((self.initial_params['color_r'],self.initial_params['color_g'],self.initial_params['color_b'],self.initial_params['color_a']))
-
+        self.pos = (self.initial_params['x'],self.initial_params['y'])
+        self.size  = (self.initial_params['width'],self.initial_params['height'])
+        self.texture = self.initial_params['texture']
         self.dormant = False
         self.dx = self.refresh_time * self.initial_params['velocity_x']
         self.dy = self.refresh_time * self.initial_params['velocity_y']
+
 
         Clock.schedule_once(self.kill,self.initial_params['lifetime'])
 
@@ -61,15 +57,12 @@ class Particle(InstructionGroup):
         if 'texture' in self.param_changes:
             self.texture_gen = (x for x in self.param_changes['texture'])
             self.update_texture(None)
-        if 'color_a' in self.param_changes:
-            self.color_a_gen = (x for x in self.param_changes['color_a'])
-            self.update_color_a(None)
         
         self.update_pos(None)
 
 
     def update_pos(self,dt):
-        self.rect.pos = (self.rect.pos[0] + self.dx, self.rect.pos[1] + self.dy)
+        self.pos = (self.pos[0] + self.dx, self.pos[1] + self.dy)
         Clock.schedule_once(self.update_pos,self.refresh_time)
 
     def update_width(self,dt):
@@ -77,7 +70,7 @@ class Particle(InstructionGroup):
             val, time = self.width_gen.next()
         except StopIteration:
             return False
-        self.rect.size = (self.rect.size[0]+val, self.rect.size[1])
+        self.size = (self.size[0]+val, self.size[1])
         Clock.schedule_once(self.update_width,time)
 
     def update_height(self,dt):
@@ -85,7 +78,7 @@ class Particle(InstructionGroup):
             val, time = self.height_gen.next()
         except StopIteration:
             return False
-        self.rect.size = (self.rect.size[0], self.rect.size[1]+val)
+        self.size = (self.size[0], self.size[1]+val)
         Clock.schedule_once(self.update_height,time)
 
     def update_texture(self,dt):
@@ -93,117 +86,115 @@ class Particle(InstructionGroup):
             val, time = self.texture_gen.next()
         except StopIteration:
             return False
-        self.rect.texture = val
+        self.texture = val
         Clock.schedule_once(self.update_texture,time)
 
-    def update_color_a(self,dt):
-        try:
-            val, time = self.color_a_gen.next()
-        except StopIteration:
-            return False
-        a = self.color_code[3]+(val/255.)
-        if a < 0: a = 0
-        self.change_color((self.color_code[0], self.color_code[1], self.color_code[2], a))
-        Clock.schedule_once(self.update_color_a,time)
-
-    def change_color(self,color_code):
-        # is there not a faster way to do this that works?
-        self.color_code = color_code
-        index = self.indexof(self.color)
-        self.remove(self.color)
-        self.color = Color(*self.color_code)
-        self.insert(index, self.color)
-
-
     def kill(self,dt):
-        # self.dormant = True
-        # Clock.unschedule(self.update_pos)
-        # Clock.unschedule(self.update_width)
-        # Clock.unschedule(self.update_texture)
-        # Clock.unschedule(self.update_height)
-        # Clock.unschedule(self.update_color_a)
+        self.dormant = True
+        Clock.unschedule(self.update_pos)
+        Clock.unschedule(self.update_width)
+        Clock.unschedule(self.update_texture)
+        Clock.unschedule(self.update_height)
         self.parent.reap_particle(self)
+
+
+
+
+
+class ParticleGroup(InstructionGroup):
+    counter = 0
+
+    def __init__(self, initial_params, param_changes = None, scatterdict = None, batch_size = 30, **kwargs):
+        super(ParticleGroup,self).__init__()
+        self.scatterdict = scatterdict if scatterdict is not None else {}
+        self.initial_params = initial_params
+        self.param_changes = param_changes if param_changes is not None else {}
+        self.batch_size = batch_size
+        self.emit_rate = initial_params['lifetime']/(batch_size*.75)
+
+        # must be called with Clock schedule, or it goes ahead and adds all these particles prematurely
+        Clock.schedule_once(self.init_particles)
+        # Clock.schedule_once(self.emit_particle,.1)
+        
+    def emit_particle(self,dt):
+        try:
+            p = self.particles.pop()
+            self.scatterdict = {'velocity_x': 'velocity_x_slider.value', 'velocity_y': velocity_y_slider.value, 'lifetime': .8}
+            p.activate()
+            self.add(p)
+        except IndexError:
+            print "empty"
+            pass
+        self.counter += 1
+        if self.counter < self.batch_size: 
+            Clock.schedule_once(self.emit_particle,self.emit_rate)
+
+    def init_particles(self,dt):
+        self.particles = deque([Particle(self.randomize_params(self.initial_params,self.scatterdict),self.param_changes,parent=self,dormant=True) for d in xrange(self.batch_size)],maxlen=self.batch_size)
+        Clock.schedule_once(self.emit_particle)
+
+    def reap_particle(self,pt):
+        self.particles.appendleft(pt)
+        self.remove(pt)
+
+    def randomize_params(self,param_dict,scatterdict):
+        params = param_dict.copy()
+        for key in params.keys():
+            try:
+                if key in scatterdict:
+                    params[key] = params[key] * uniform(1-scatterdict[key],1+scatterdict[key]) if scatterdict[key] < 1 else params[key] + randint(int(-scatterdict[key]),int(scatterdict[key]))
+                else:
+                    params[key] *= uniform(0.9,1.1)
+            except:
+                pass
+        return params
 
 
 class ParticleEmitter(Widget):
 
-    def __init__(self, emmitter_type = 'smoke', num_particles = 30, **kwargs):
-        super(ParticleEmitter,self).__init__(**kwargs)
-        assert emmitter_type in ['smoke', 'snow']
-        self.emmitter_type = emmitter_type
-        self.num_particles = num_particles
-        self.counter = 0
-        Clock.schedule_once(self.setup_window)
-        
-        
+    
+    # start_btn.bind(on_press=ParticleEmitter(image_basename = 'VFX-Explosion', image_location = 'VFX/VFX_Set1_64.atlas', debug=db_panel, pos=(200,200),size=(128,128),size_hint=(None,None)))
 
-    def setup_window(self,dt):
-        print self.size
-        if self.emmitter_type == 'smoke':
+    def __init__(self, image_basename, image_location, emmitter_type = 'smoke', batches = 10, batch_size = 100,**kwargs):
+        super(ParticleEmitter,self).__init__()
+        assert emmitter_type in ['smoke']
+
+        if emmitter_type == 'smoke':
             texture_list = self.get_texture_list('VFX-Explosion','VFX/VFX_Set1_64.atlas')
-            self.initial_params = {'velocity_x': 0,
-                                    'velocity_y': 200,
-                                    'texture': texture_list[0],
-                                    'x': self.pos[0],
-                                    'y': self.pos[1],
-                                    'lifetime': 1.0,
-                                    'width': 64,
-                                    'height': 64,
-                                    'color_r': 0.,
-                                    'color_g': 0.,
-                                    'color_b': 1.,
-                                    'color_a': 1.,}
-            self.param_changes = {'texture': zip(texture_list[1:],[0.1]*(len(texture_list)-1)),
+            self.initial_params = {'velocity_x': velocity_x_slider.value,
+                'velocity_y': velocity_y_slider.value,
+                'texture': texture_list[0],
+                'x': 200,
+                'y': 200,
+                'lifetime': 4.,
+                'width': 64,
+                'height': 64,}
+            self.param_changes = {'texture': zip(texture_list[1:],[0.6]*(len(texture_list)-1)),
                                 'width': [(8,.5),(8,.5),(8,.5),(8,.25),(8,.25),(8,.25),(8,.25)],
-                                'height': [(8,.5),(8,.5),(8,.5),(8,.25),(8,.25),(8,.25),(8,.25),],
-                                'color_a': [(-30,.2),(-30,.2),(-30,.2),(-30,.2),(-50,.2),]}
-            self.scatterdict = {'velocity_x': 100, 
-                            'velocity_y': 20, 
-                            'lifetime': .1, 
-                            'x': 10,
-                            'y': 10}
+                                'height': [(8,.5),(8,.5),(8,.5),(8,.25),(8,.25),(8,.25),(8,.25),]}
+            self.scatterdict = {'velocity_x': 'velocity_x_slider.value', 'velocity_y': 200, 'lifetime': .8}
+            self.batch_size = batch_size
 
-        elif self.emmitter_type == 'snow':
-            texture_list = self.get_texture_list('VFX_SnowFlake_','VFX/VFX_Set1.atlas')
-            self.initial_params = {'velocity_x': 0,
-                                    'velocity_y': -75,
-                                    'texture': texture_list[0],
-                                    'x': self.x + self.width/2.,
-                                    'y': self.top,
-                                    'lifetime': 10.0,
-                                    'width': 16,
-                                    'height': 16,
-                                    'color_r': 1.,
-                                    'color_g': 1.,
-                                    'color_b': 1.,
-                                    'color_a': 1.,}
-            self.param_changes = {'texture': zip(texture_list[1:],[2.0]*(len(texture_list)-1)),
-                                'width': [(8,.5),(8,.5),(8,.5),(8,.25),(8,.25),(8,.25),(8,.25)],
-                                'height': [(8,.5),(8,.5),(8,.5),(8,.25),(8,.25),(8,.25),(8,.25),],}
-                                # 'color_a': [(-50,.2),(-50,.2),(-50,.2),(-50,.2),(-50,.2),]}
-            self.scatterdict = {'velocity_x': 10, 
-                            'velocity_y': 25, 
-                            'lifetime': .33, 
-                            'x': self.width * .5,
-                            'y': 10,
-                            'color_r': 0,
-                            'color_g': 0,
-                            'color_b': 0,
-                            'color_a': .1,}
-        
+        start_btn = Button(text = 'Start Emitter', font_size = 14, size = (100,100), size_hint = (None,None), pos = (600,50))
+        self.add_widget(start_btn)
+        start_btn.bind(on_press=self.initiate)
 
-        self.slate = InstructionGroup()
-        self.canvas.add(self.slate)
+    def initiate(self,instance):
+        batches = 10
+        initial_params = self.initial_params
+        param_changes = self.param_changes
+        scatterdict = {'velocity_x': velocity_x_slider.value, 'velocity_y': velocity_y_slider.value, 'lifetime': .8}
+        batch_size = self.batch_size 
 
-        self.emit_rate = self.initial_params['lifetime']/float(self.num_particles*1.1)
-        if self.emit_rate < 0.02: self.emit_rate = 0.02
-        Clock.schedule_once(self.emit_particle)
+        for b in range(batches):
+            self.emit_particle_group(initial_params, param_changes, scatterdict, batch_size)
+    
 
-    def emit_particle(self, dt):
-        self.slate.add(Particle(self.randomize_params(),self.param_changes,parent=self,dormant=False))
-            # p = ParticleGroup(initial_params, param_changes=param_changes, scatterdict=scatterdict, batch_size = batch_size)
-        # self.counter += 1
-        if self.counter < self.num_particles: Clock.schedule_once(self.emit_particle,self.emit_rate)
+
+
+    def emit_particle_group(self, initial_params, param_changes, scatterdict, batch_size):
+        with self.canvas:
+            p = ParticleGroup(initial_params, param_changes=param_changes, scatterdict=scatterdict, batch_size = batch_size)
 
     def get_texture_list(self, image_basename, image_location):
         basename = image_basename
@@ -213,28 +204,11 @@ class ParticleEmitter(Widget):
 
         if len(wanted_keys) > 1:
             pattern = re.compile(r'[0-9]+$')
+            print wanted_keys
             wanted_keys.sort(key=lambda x: int(pattern.search(x).group()))
 
         return [atlas[key] for key in wanted_keys]
 
-    def reap_particle(self,particle):
-        # these lines are only necessary if particle is moving or we want to alter effect. we will want to find a way to not run them in other cases.
-        # particle.initial_params = self.randomize_params()
-        # particle.param_changes = self.param_changes
-        # particle.activate()
-        self.slate.remove(particle)
-
-    def randomize_params(self):
-        params = self.initial_params.copy()
-        for key in params.keys():
-            try:
-                if key in self.scatterdict:
-                    params[key] = params[key] * uniform(1-self.scatterdict[key],1+self.scatterdict[key]) if self.scatterdict[key] < 1 else params[key] + randint(int(-self.scatterdict[key]),int(self.scatterdict[key]))
-                else:
-                    params[key] *= uniform(0.9,1.1)
-            except:
-                pass
-        return params
 
 class CBL_DebugPanel(BoxLayout):
     fps = StringProperty(None)
@@ -248,15 +222,19 @@ class CBL_DebugPanel(BoxLayout):
         self.fps = str(int(Clock.get_fps()))
         Clock.schedule_once(self.update)
 
-
 class LowLevelParticleEngineApp(App):
     def build(self):
         # 'atlas://VFX/smoke_particles/VFX_SmokeParticle'
         fl = FloatLayout(pos=(0,0),size=Window.size)
         db_panel = CBL_DebugPanel(pos=(0,0),size=(100,50),size_hint = (None,None))
         fl.add_widget(db_panel)
-        for d in xrange(1):
-            fl.add_widget(ParticleEmitter(emmitter_type = 'snow', num_particles=15, debug=db_panel,size_hint=(1,1)))
+        global velocity_x_slider
+        velocity_x_slider = Slider(min=0,max=200,value=20,center=(650,600),size=(150,10),size_hint=(None,None))
+        fl.add_widget(velocity_x_slider)
+        global velocity_y_slider
+        velocity_y_slider = Slider(min=0,max=200,value=20,center=(650,550),size=(150,10),size_hint=(None,None))
+        fl.add_widget(velocity_y_slider)
+        fl.add_widget(ParticleEmitter(image_basename = 'VFX-Explosion', image_location = 'VFX/VFX_Set1_64.atlas', debug=db_panel, pos=(200,200),size=(128,128),size_hint=(None,None)))
         return fl
 
 
